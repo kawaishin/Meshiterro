f32e55e7ccecc06b5332fa1d22bf833777366105
kawaishin <believeshin0410@gmail.com>
2020-01-20 00:50:11 +0000

    Initial commit message

---
+++ config/initializers/application_controller_renderer.rb
@@ -0,0 +1,9 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+Refile.secret_key = 'd07d93edddaeece6c9a1b419a5dcb494a422261a4fda5edeb779f52e783ed5dccc3e2c5b4651b199ac90d64f1f903c597a2f86b7104faf90c68131b99d29fde8'
